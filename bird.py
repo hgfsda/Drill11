ab0887b92772553d963f48186b1c151b42af9452
Jeong tae yeon <jtyk119@naver.com>
2023-11-02 11:13:13 +0900

    10마리의 새때 랜덤한 위치에 생성

--- bird.py
+++ bird.py
@@ -2,19 +2,20 @@
 
 from pico2d import get_time, load_image, load_font, clamp
 import game_framework
+import random
 
 # state event check
 # ( state event type, event value )
 
 
-# Boy Run Speed
+# bird Run Speed
 # fill here
 PIXEL_PER_METER = (10.0 / 0.3)
 RUN_SPEED_KMPH = 20.0
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
-# Boy Action Speed
+# bird Action Speed
 # fill here
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
@@ -24,51 +25,51 @@ FRAMES_PER_ACTION = 8
 class Run:
 
     @staticmethod
-    def enter(boy, e):
+    def enter(bird, e):
         pass
 
     @staticmethod
-    def exit(boy, e):
+    def exit(bird, e):
         pass
 
     @staticmethod
-    def do(boy):
-        boy.frame = (boy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
-        boy.x += boy.dir * RUN_SPEED_PPS * game_framework.frame_time
-        boy.x = clamp(25, boy.x, 1600-25)
+    def do(bird):
+        bird.frame = (bird.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
+        bird.x += bird.dir * RUN_SPEED_PPS * game_framework.frame_time
+        bird.x = clamp(25, bird.x, 1600-25)
 
 
     @staticmethod
-    def draw(boy):
-        boy.image.clip_draw(int(boy.frame) * 100, boy.action * 100, 100, 100, boy.x, boy.y)
+    def draw(bird):
+        bird.image.clip_composite_draw(int(bird.frame) * 183, 336, 180, 168, 0, 'h', bird.x, bird.y, 135, 135)
 
 
 class StateMachine:
-    def __init__(self, boy):
-        self.boy = boy
+    def __init__(self, bird):
+        self.bird = bird
         self.cur_state = Run
         self.transitions = {
             Run: {},
         }
 
     def start(self):
-        self.cur_state.enter(self.boy, ('NONE', 0))
+        self.cur_state.enter(self.bird, ('NONE', 0))
 
     def update(self):
-        self.cur_state.do(self.boy)
+        self.cur_state.do(self.bird)
 
     def handle_event(self, e):
         for check_event, next_state in self.transitions[self.cur_state].items():
             if check_event(e):
-                self.cur_state.exit(self.boy, e)
+                self.cur_state.exit(self.bird, e)
                 self.cur_state = next_state
-                self.cur_state.enter(self.boy, e)
+                self.cur_state.enter(self.bird, e)
                 return True
 
         return False
 
     def draw(self):
-        self.cur_state.draw(self.boy)
+        self.cur_state.draw(self.bird)
 
 
 
@@ -76,7 +77,7 @@ class StateMachine:
 
 class Bird:
     def __init__(self):
-        self.x, self.y = 400, 90
+        self.x, self.y = random.randint(100, 1600), random.randint(300, 500)
         self.frame = 0
         self.action = 3
         self.face_dir = 1
